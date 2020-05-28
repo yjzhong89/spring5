5c41b0fd934624152b1ca03143c61ab260f8ea1f
yjzhong <hustzyj89@gmail.com>
2020-05-28 17:43:28 +0800

    rename circular test class

--- spring-zyj/src/main/java/com/zyj/spring/circularTest/CircularTest.java
+++ spring-zyj/src/main/java/com/zyj/spring/circularTest/CircularTest.java
@@ -3,7 +3,7 @@ package com.zyj.spring.circularTest;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test {
+public class CircularTest {
 	public static void main(String[] args) {
 		BeanFactory factory = new ClassPathXmlApplicationContext("circularTest.xml");
 	}
